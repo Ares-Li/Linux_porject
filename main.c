73b383d733ff256241529f48fd59f2c36abdbfdd
An <An@337345043qq.com>
2018-08-28 16:58:18 +0800

    add annotaion

--- main.c
+++ main.c
@@ -6,3 +6,26 @@ int main()
 
 	return 0;
 }
+/*
+git init
+
+git status
+
+git log
+git reflog
+
+git reset --hard HEAD^
+git reset --hard HEAD~1
+git reset --hard 'id'
+
+
+git add 'name'
+git rm 'name'
+
+git commit -m 'xxxxxx'
+
+git checkout -- 'name'
+
+8/
+
+
